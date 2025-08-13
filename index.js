7954f9573fa9911c6f70442ea6304c5ef773213a
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-08-13 20:40:53 +0700

    menjalankan

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const dotenv = require('dotenv');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
-const { GoogleGenerativeAI } = require('google-generativeai');
+const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'models/gemini-2.0-flash' });
+const model = genAI.getGenerativeModel({ model: 'models/gemini-2.5-flash' });
 
 const upload = multer({ dest: 'uploads/' });
 
@@ -18,3 +18,15 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+app.post('/generate-text', async (req, res) => {
+    const { prompt } = req.body;
+
+    try {
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        res.json({ output: response.text()})
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
